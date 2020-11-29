772d6b5f43442116b4734b0a2160b48bcb0435c8
yfijixxx <20085584+fijixxx@users.noreply.github.com>
2020-11-29 22:02:08 +0900

    一覧と記事詳細をスタイリング

---
+++ components/About.tsx
@@ -0,0 +1,11 @@
+import { Center, Box, Code } from "@chakra-ui/react";
+
+const About = (): JSX.Element => (
+  <Center>
+    <Box fontWeight="semibold" ad="h4" mt="4" mb="4">
+      <Code>sublog.yfijixxx</Code>
+    </Box>
+  </Center>
+);
+
+export default About;
