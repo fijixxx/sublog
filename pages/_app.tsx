ad90931e8f8103c341e085770b0f34d04c11624a
yfijixxx <20085584+fijixxx@users.noreply.github.com>
2020-12-02 23:10:36 +0900

    とりあえず https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics コピペ

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,37 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-      />
-      {/* <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" /> */}
-      {/* <link rel="manifest" href="/manifest.json" /> */}
-    </Head>
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  </>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {/* <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" /> */}
+        {/* <link rel="manifest" href="/manifest.json" /> */}
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
+};
 
 export default App;
