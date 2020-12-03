23bb13a4eb0603ad109cfd526eaec0a79e84e1fa
yfijixxx <20085584+fijixxx@users.noreply.github.com>
2020-12-04 00:09:12 +0900

    github-markdown-css(https://github.com/sindresorhus/github-markdown-css/blob/gh-pages/github-markdown.css)を参考に、一部だけ暫定対応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import * as gtag from "../lib/gtag";
+import { extendTheme } from "@chakra-ui/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
@@ -17,6 +18,49 @@ const App = ({ Component, pageProps }: AppProps) => {
     };
   }, [router.events]);
 
+  const theme4ParsedMD = extendTheme({
+    styles: {
+      global: {
+        h1: { fontSize: "3xl", fontWeight: "600" },
+        h2: { fontSize: "xl", fontWeight: "600" },
+        h3: { fontSize: "md", fontWeight: "600" },
+        h4: { fontSize: "sm", fontWeight: "600" },
+        h5: { fontSize: "xs", fontWeight: "600" },
+        li: {
+          ul: {
+            paddingLeft: "28px",
+            ul: {
+              paddingLeft: "28px",
+            },
+          },
+        },
+        pre: {
+          fontFamily: "Consolas",
+          fontSize: "85%",
+          bg: "#f6f8fa",
+          padding: "16px",
+        },
+        img: {
+          margin: "0 auto",
+        },
+        p: {
+          code: {
+            fontFamily: "Consolas",
+            bg: "#f6f8fa",
+            p: "0 2.8px",
+          },
+        },
+        blockquote: {
+          p: {
+            color: "#6a737d",
+            padding: "0 8px",
+            borderLeft: "4px solid #dfe2e5",
+          },
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Head>
@@ -27,7 +71,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         {/* <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" /> */}
         {/* <link rel="manifest" href="/manifest.json" /> */}
       </Head>
-      <ChakraProvider>
+      <ChakraProvider theme={theme4ParsedMD}>
         <Component {...pageProps} />
       </ChakraProvider>
     </>
